8412e280448236361499e2fc285020f6ea11e89b
designMoreWeb <divystolia@gmail.com>
2018-12-11 17:29:44 -0500

    no console errors!

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const App = () =>{
     const labelText = 'Enter Name : ';
     return (
         <div>
-            <label className ="label" for="name">{labelText}</label>
+            <label className ="label" htmlFor="name">{labelText}</label>
             <input id ="name" type="text" />
             <button style ={{backgroundColor:'blue',color:'white'}}>{buttonText.text}</button>
         </div>
